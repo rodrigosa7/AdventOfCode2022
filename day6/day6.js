739a52c1ac0ef766261847700fb7019bd29fafe7
rodrigosa7 <rodrigo.gomes.sa@outlook.pt>
2022-12-06 12:09:13 +0000

    Day 6 solution

---
+++ day6/day6.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataStream = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+
+const checkDifferentChars = (input) => {
+    return /(.).*\1/.test(input);
+}
+
+const validate = (input, size) => {
+    let index = 0;
+    for(let i = 0; i < input.length; i++) {
+        let maxRange = i + size;
+        if(!checkDifferentChars(input.substring(i, maxRange))) {
+            index = maxRange;
+            break;
+        }
+    }
+    return index
+}
+
+console.log(validate(dataStream, 4));
+console.log(validate(dataStream, 14));
